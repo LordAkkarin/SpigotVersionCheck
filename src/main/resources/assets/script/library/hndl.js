7b443abc07e64a8062615f7e498bb4a71d46ef95
Johannes Donath <johannesd@evil-co.com>
2014-12-13 14:41:51 +0100

    Fixed typo.

--- src/main/resources/assets/script/library/hndl.js
+++ src/main/resources/assets/script/library/hndl.js
@@ -14,6 +14,21 @@
  */
 
 /*
+ * Copyright 2014 Johannes Donath <johannesd@torchmind.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Copyright (C) 2014 Johannes Donath <johannesd@evil-co.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -55,7 +70,7 @@ define (['handlebars'], function (Handlebars) {
 		 * @param require The parent require instance.
 		 * @param onLoad Plugin callback.
 		 * @param config The plugin configuration.
-		 * @todo RequireJS currently cannot execut async operations during optimization.
+		 * @todo RequireJS currently cannot execute async operations during optimization.
 		 */
 		load:			function (name, require, onLoad, config) {
 			// stop optimizer
