cd1136faeda3bbc8de1e604f9dd05897147d07b4
Johannes Donath <johannesd@evil-co.com>
2014-12-13 19:25:26 +0100

    Tell the world that we messed up ...

--- src/main/java/rocks/spud/spigot/data/stash/StashCommitResponse.java
+++ src/main/java/rocks/spud/spigot/data/stash/StashCommitResponse.java
@@ -25,6 +25,7 @@ import java.util.List;
  * Provides a representation for Stash commit API responses.
  * @author Johannes Donath <johannesd@torchmind.com>
  * @copyright Copyright (C) 2014 Torchmind <http://www.torchmind.com>
+ * @todo This system currently relies on Stash's commit ordering. However this does not always work out!
  */
 @JsonIgnoreProperties (ignoreUnknown = true)
 public class StashCommitResponse {
