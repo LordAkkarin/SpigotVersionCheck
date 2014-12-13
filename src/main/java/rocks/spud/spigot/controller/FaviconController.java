a83d0558c83acec2d7612f7f7db03bf63e67760b
Johannes Donath <johannesd@evil-co.com>
2014-12-13 01:32:43 +0100

    Added a favicon controller.

---
+++ src/main/java/rocks/spud/spigot/controller/FaviconController.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@torchmind.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package rocks.spud.spigot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Johannes Donath <johannesd@torchmind.com>
+ * @copyright Copyright (C) 2014 Torchmind <http://www.torchmind.com>
+ */
+@Controller
+public class FaviconController {
+
+	/**
+	 * Serves the favicon.
+	 * @return The favicon template.
+	 */
+	@RequestMapping (value = "favicon.ico", method = RequestMethod.GET)
+	public String favicon () {
+		return "forward:/assets/favicon.ico";
+	}
+}
