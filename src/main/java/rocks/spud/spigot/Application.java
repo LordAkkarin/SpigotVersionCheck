0926445a1834cf707504d35d091fa9fb183c4971
Johannes Donath <johannesd@evil-co.com>
2014-12-13 01:28:42 +0100

    Added root application class.

---
+++ src/main/java/rocks/spud/spigot/Application.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@torchmind.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package rocks.spud.spigot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Main application configuration.
+ * @author Johannes Donath <johannesd@torchmind.com>
+ * @copyright Copyright (C) 2014 Torchmind <http://www.torchmind.com>
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+	/**
+	 * Main Entry Point
+	 * @param arguments The command line arguments.
+	 */
+	public static void main (String[] arguments) {
+		// run application
+		SpringApplication.run (Application.class, arguments);
+	}
+}
